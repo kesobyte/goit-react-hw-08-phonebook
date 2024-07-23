d8388260e3953021aa9ee9c6ac844c419ff49087
kesobyte <kimangeloalvarez@gmail.com>
2024-07-23 11:28:53 +0800

    template/css update

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,11 @@
-import css from './Filter.module.css';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from '../../redux/filter/filterSlice';
 import { selectFilter } from '../../redux/selectors';
 import { DebounceInput } from 'react-debounce-input';
+import { InputGroup, InputLeftElement, Input, Stack } from '@chakra-ui/react';
+import { FaSearch } from 'react-icons/fa';
+import css from './Filter.module.css';
 
 export const Filter = () => {
   const dispatch = useDispatch();
@@ -13,16 +16,23 @@ export const Filter = () => {
   };
 
   return (
-    <>
-      <DebounceInput
-        className={css.searchBox}
-        type="text"
-        name="filter"
-        placeholder="Search by name"
-        value={filter}
-        onChange={handleFilterChange}
-        debounceTimeout={500}
-      />
-    </>
+    <Stack spacing={4}>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <FaSearch color="gray" />
+        </InputLeftElement>
+        <DebounceInput
+          element={Input}
+          type="text"
+          name="filter"
+          placeholder="Search by name"
+          value={filter}
+          onChange={handleFilterChange}
+          debounceTimeout={500}
+          className={css.searchBox}
+          focusBorderColor="#5046e5"
+        />
+      </InputGroup>
+    </Stack>
   );
 };
